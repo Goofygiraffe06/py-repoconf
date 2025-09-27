53f4060e2b7bf8d5f2d991b4cd32f26efa5b268e
Suhas Krishna Srivastava <suhasCanDoSo@gmail.com>
2025-09-28 00:17:38 +0530

    Added core.

--- src/repoconf/__init__.py
+++ src/repoconf/__init__.py
@@ -4,9 +4,11 @@
 Create, edit, set, unset, ... etc. the repo's central configurations. These configurations are stored in a branch which
 can then be shared with all the repo's collaborators.
 """
+import logging
+
 from logician.configurators.env import LgcnEnvListLC
-from logician.std_log.configurator import StdLoggerConfigurator
+from logician.stdlog.configurator import StdLoggerConfigurator, VQCommLoggerConfigurator
 
 REPOCONF_LOG_ENV = "REPOCONF_LOG"
 
-repoconf_lc = LgcnEnvListLC([REPOCONF_LOG_ENV], StdLoggerConfigurator())
+repoconf_lc = LgcnEnvListLC([REPOCONF_LOG_ENV], VQCommLoggerConfigurator(None, StdLoggerConfigurator()))
--- src/repoconf/core/__init__.py
+++ src/repoconf/core/__init__.py
@@ -1,3 +1,17 @@
 """
 Library core of ``repoconf``.
 """
+import logging
+
+import repoconf
+
+rclc = repoconf.repoconf_lc.clone_with_envs(f"{repoconf.REPOCONF_LOG_ENV}_CORE")
+rclc_log = logging.getLogger(__name__)
+rclc_logger = rclc.configure(rclc_log)
+
+
+def main():
+    rclc_logger.success("repoconf")
+
+if __name__ == '__main__':
+    main()
