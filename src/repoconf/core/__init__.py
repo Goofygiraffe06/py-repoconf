b8da3442dff67ac17e8ac43fa5ba557185f878b1
Suhas Krishna Srivastava <suhasCanDoSo@gmail.com>
2025-09-30 22:38:44 +0530

    Committing development.

--- src/repoconf/core/__init__.py
+++ src/repoconf/core/__init__.py
@@ -5,6 +5,7 @@ Library core of ``repoconf``.
 import logging
 
 import repoconf
+import gitbolt
 
 rclc = repoconf.repoconf_lc.clone_with_envs(f"{repoconf.REPOCONF_LOG_ENV}_CORE")
 rclc_log = logging.getLogger(__name__)
@@ -12,6 +13,8 @@ rclc_logger = rclc.configure(rclc_log)
 
 
 def main():
+    git = gitbolt.get_git()
+    print(git.version)
     rclc_logger.success("repoconf")
 
 
